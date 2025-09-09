586baddf7a6fc6c7bea77f11ad83d34d87242dc1
JonathanZ <zez451@usask.ca>
2025-09-08 22:17:16 -0600

    lot of tasks together, all sleep tasks

--- hello_async.py
+++ hello_async.py
@@ -15,6 +15,13 @@ async def main():
     await asyncio.gather(
         async_task("A", 21),
         async_task("B", 12),
+        async_task("C", 11),
+        async_task("D", 10),
+        async_task("E", 15),
+        async_task("F", 12),
+        async_task("G", 13),
+        async_task("H", 12),
+        async_task("I", 15),
         hello_async()
     )
     print("all completed.")
@@ -32,4 +39,6 @@ if __name__ == "__main__":
 # A -> 4s, B -> 4s, total -> 4.0118 s
 # A -> 10s, B -> 14s, total -> 14.0162 s
 # B -> 12s, A -> 21s, total -> 21.0092 s
+
+# executing a lot of sleep-tasks together -> 21.0124 s
 
