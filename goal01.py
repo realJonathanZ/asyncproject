35daa17682968eb9c5eaa767c5d414439a6c3860
JonathanZ <zez451@usask.ca>
2025-09-11 23:21:43 -0600

    minor change

--- goal01.py
+++ goal01.py
@@ -1,11 +1,11 @@
-# For this file, simulated (arbitrary) numbers of api calls to deal with.
-# The api call response time varies from 10 to 15 seconds.
-# The calls are to made concurrently.
-# Predefined array containing 0s and 1s that quantity is not fixed.
-# Search the first one in array -> make an api call -> replace 1 with 0 -> loop
-# until all entries are 0.
+'''For this file, simulated (arbitrary) numbers of api calls to deal with.
+The api call response time varies from 10 to 15 seconds.
+The calls are to made concurrently.
+Predefined array containing 0s and 1s that quantity is not fixed.
+Search the first one in array -> make an api call -> replace 1 with 0 -> loop
+until all entries are 0.'''
 
-# And more importantly, The (n+1)th call must start after nth call has started.
+And more importantly, The (n+1)th call must start after nth call has started.```
 
 import asyncio
 import random
